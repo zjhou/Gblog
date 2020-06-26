70e7b5ab8192d31f6b16838a89401382ca8cf8f5
ZjHOU <z@zjh.im>
2020-06-26 11:54:09 +0800

    fix: cors

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,13 @@
+const cors = require('micro-cors')();
 const Blog = require('../backyard/blog');
 const blog = new Blog();
 
-module.exports = async (req, res) => {
+const handler = async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return res.status(200).send('ok');
+  }
   const posts = await blog.getPosts();
   res.json(posts).status(200);
 };
+
+module.exports = cors(handler);
